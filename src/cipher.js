219f3f977e43cb770f65494ea7e7a4164d84dd78
Karen Ramírez Legorreta <karenramirezlegorreta2@gmail.com>
2019-01-23 20:44:39 -0600

    comentario 23 de enero

--- src/cipher.js
+++ src/cipher.js
@@ -17,17 +17,34 @@ encode: (offsetIngresado, messageIngresado) => {
 
 
       messageImpreso+=messageCipher;
-     }
 
-//console.log(messageImpreso)
+    }
+
 return messageImpreso
-}
-}
-// },
-//
-// decode: => {}
-//
-//
-//
-//
-// }
+
+},
+
+
+decode: (offsetIngresado, messageIngresado) => {
+
+   let messageImpreso= "";
+   let i = 0;
+   let letraMayus= messageIngresado.toUpperCase();
+
+  // console.log(letraMayus)
+
+   //messageIngresado= messageIngresado.toUpperCase();
+
+    for (i=0; i<letraMayus.length; i++) {
+
+       let letraAscii= letraMayus.charCodeAt(i);
+       let formula= (letraAscii + 65 - parseInt(offsetIngresado)) %26 + 65;
+       let messageDeCipher= String.fromCharCode(formula);
+
+
+      messageImpreso+=messageDeCipher;
+
+    }
+  return messageImpreso
+    }
+    }
--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ document.getElementById("bottonCipher").addEventListener("click",()=>{
   let codificado= window.cipher.encode(offsetIngresado, messageIngresado);
   document.getElementById('reemplazo').innerHTML = codificado;
 });
+
+document.getElementById("bottonDecipher").addEventListener("click",()=>{
+
+  let offsetIngresado= document.getElementById("offset").value;
+  let messageIngresado= document.getElementById("message").value;
+  let codificado2= window.cipher.decode(offsetIngresado, messageIngresado);
+  document.getElementById('reemplazo').innerHTML = codificado2;
+});
