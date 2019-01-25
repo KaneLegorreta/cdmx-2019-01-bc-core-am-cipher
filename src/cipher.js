e360eaa2bc4847ed0a4c699386b0fd438c0ef06f
Karen Ramírez Legorreta <karenramirezlegorreta2@gmail.com>
2019-01-25 01:47:38 -0600

    ULTIMO COMMIT jueves madrugada

--- src/cipher.js
+++ src/cipher.js
@@ -8,14 +8,18 @@ encode: (offsetIngresado, messageIngresado) => {
   // console.log(letraMayus)
 
    //messageIngresado= messageIngresado.toUpperCase();
-//
+   //messageIngresado= messageIngresado.toUpperCase();
+//bucle for comienza iterando letra por letra , la pasa por la variable "letra ascii " y la convierte a codigo ascii ,
+//despues la pasa por la variable de  nombre "formula= letra ascii +65 - parseInt", lo cual hace que se sume el offset ingresado
+//y tendra un nuevo valor que sera una posicion en codigo ascii
     for (i=0; i<letraMayus.length; i++) {
 
        let letraAscii= letraMayus.charCodeAt(i);
        let formula= (letraAscii - 65 + parseInt(offsetIngresado)) %26 + 65;
        let messageCipher= String.fromCharCode(formula);
 
-
+       //aun dentro de for, pasa por la variable "string.fromCharCode" para buscar e identificar la letra que tiene esa posicion en ascii
+       //
       messageImpreso+=messageCipher;
 
     }
@@ -33,13 +37,14 @@ decode: (offsetIngresado, messageIngresado) => {
 
   // console.log(letraMayus)
 
-   //messageIngresado= messageIngresado.toUpperCase();
 
     for (i=0; i<letraMayus.length; i++) {
 
        let letraAscii= letraMayus.charCodeAt(i);
        let formula= (letraAscii + 65 - parseInt(offsetIngresado)) %26 + 65;
        let messageDeCipher= String.fromCharCode(formula);
+
+
 
 
       messageImpreso+=messageDeCipher;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 //pedir al documento que me invoque el id del botton de cifrar de nombre "bottonCipher"
 //El addEventListener dispara un evento de tipo click, significa que cuando de click
-// me realiza la funcion que este dentro de click
+//  realiza la funcion que este dentro de click
 document.getElementById("bottonCipher").addEventListener("click",()=>{
 
   let offsetIngresado= document.getElementById("offset").value;
